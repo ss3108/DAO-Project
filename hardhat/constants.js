d35300fb8a05438699b4313d0c6bd624cd1c90d5
Shruti Singh <shrutisingh3108@gmail.com>
2022-11-16 13:43:20 +0530

    website

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x85f1fda3E6f093B5c12642193962660F1b732C2b";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
